5bb8a8dfb2a21eab34c7f6acae05c8290a7155da
abhi195 <abhisapariya@gmail.com>
2021-01-24 20:58:31 +0530

    local metrics reporting

--- server/middleware.go
+++ server/middleware.go
@@ -9,6 +9,8 @@ import (
 func loggingMiddlewareHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		m := httpsnoop.CaptureMetrics(h, w, r)
+		requestCounterInc(r.URL.Path, r.Method, m.Code)
+		requestLatencyUpdate(r.URL.Path, r.Method, m.Code, m.Duration)
 		logger.Infof(
 			"%s %s (code=%d dt=%s written=%d)",
 			r.Method,
